503e1564c222d17ada244a30c824b1bc4b1f7af4
PhanVanDiej <22520182@gm.uit.edu.vn>
2023-12-20 22:17:53 +0700

    Dai update

---
+++ HealthCareApp/HealthCareApp/Models/SymptomCheckerModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareApp.Models
+{
+    internal class SymptomCheckerModel
+    {
+        public class symptom
+        {
+            public string ID { get; set; }
+            public string Name { get; set; }
+        }
+        public class Autherity
+        {
+            public string Token { get; set; }
+            public int ValidThrough { get; set; }
+        }
+        public class rootSymptoms
+        {
+            public List<symptom> symptoms = new List<symptom>();
+        }
+    }
+}
