b5d53a6340c4cb6917324f33fc451ef7f69439d6
PhanVanDiej <22520182@gm.uit.edu.vn>
2023-12-24 23:48:28 +0700

    update Nutrient

--- HealthCareApp/HealthCareApp/ViewModels/NutrientsViewModel.cs
+++ HealthCareApp/HealthCareApp/ViewModels/NutrientsViewModel.cs
@@ -11,14 +11,27 @@ using System.Windows;
 using System.Net.Http.Json;
 using HealthCareApp.Models;
 using System.IO;
+using System.Windows.Input;
+using HealthCareApp.Views;
 
 namespace HealthCareApp.ViewModels
 {
     public class NutrientsViewModel : BaseViewModel
     {
+        public ICommand AddMealCommand { get; set; }
+        public ICommand CalculateCommand { get; set; }
+
         public NutrientsViewModel()
         {
-
+            AddMealCommand=new RelayCommand<NutrientsView>((p) => { return true; },(p)=> {
+                AddMealView addMealView = new AddMealView();
+                addMealView.ShowDialog();
+            });
+            CalculateCommand = new RelayCommand<NutrientsView>((p) => { return true; }, (p) =>
+            {
+                CalculateCaloriesView calculateCaloriesView = new CalculateCaloriesView();
+                calculateCaloriesView.ShowDialog();
+            });
         }
     }
 }
