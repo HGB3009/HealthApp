aa0a95f9c70b3b0bfc592eff17d92947ee90a3d8
HGB3009 <22520103@gm.uit.edu.vn>
2023-12-26 16:51:56 +0700

    update forgot password

--- HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
+++ HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4ef490b9f0b723274f308ed213edf35282f893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
