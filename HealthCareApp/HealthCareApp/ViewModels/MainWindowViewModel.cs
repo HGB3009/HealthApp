b71136993f43fc12eb32ef1900944df08cbb9123
HGB3009 <22520103@gm.uit.edu.vn>
2023-12-29 02:54:11 +0700

    navigation update2

--- HealthCareApp/HealthCareApp/ViewModels/MainWindowViewModel.cs
+++ HealthCareApp/HealthCareApp/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows;
+using HealthCareApp.Components;
 
 namespace HealthCareApp.ViewModels
 {
@@ -31,20 +32,35 @@ namespace HealthCareApp.ViewModels
         public ICommand DiagnosisFieldCommand { get; set; }
         public ICommand RemindersFieldCommand { get; set; }
         public ICommand SettingFieldCommand { get; set; }
+        public ICommand LogoutMainWD { get; set; }
 
         public MainWindowViewModel()
         {
             MenuFieldCommand = new RelayCommand(MainMenu);
             FitnessFieldCommand = new RelayCommand(Fitness);
             SleepFieldCommand = new RelayCommand(Sleep);
+            NutritionFieldCommand = new RelayCommand(Nutrition);
+            DiagnosisFieldCommand = new RelayCommand(Diagnosis);
             RemindersFieldCommand = new RelayCommand(Reminders);
             SettingFieldCommand = new RelayCommand(Settings);
             CurrentView = new MenuView();
+            LogoutMainWD = new RelayCommand<LeftSideBarView>((p) => true, (p) => _Logout(p));
         }
         private void MainMenu(object obj) => CurrentView = new MenuView();
         private void Fitness(object obj) => CurrentView = new FitnessView();
         private void Sleep(object obj) => CurrentView = new SleepView();
+        private void Nutrition(object obj) => CurrentView = new NutritionView();
+        private void Diagnosis(object obj) => CurrentView = new DiagnosisView();
         private void Reminders(object obj) => CurrentView = new RemindersView();
         private void Settings(object obj) => CurrentView = new SettingsView();
+        void _Logout(LeftSideBarView mainWindow)
+        {
+
+            var window = Window.GetWindow(mainWindow);
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
     }
 }
