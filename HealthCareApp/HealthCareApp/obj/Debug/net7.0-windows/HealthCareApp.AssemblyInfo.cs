5cb8411bd8805655b9e18ead032fc243c9277fbb
PhanVanDiej <22520182@gm.uit.edu.vn>
2023-12-30 22:32:56 +0700

    update diagnosis

--- HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
+++ HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7edb905c1da7442af427ec6b10b079bc7e190eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
