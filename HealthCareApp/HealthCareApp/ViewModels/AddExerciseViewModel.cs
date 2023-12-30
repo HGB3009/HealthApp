a76d402c0ceb872ad2338a93cc4aa310e96f6661
PhanVanDiej <22520182@gm.uit.edu.vn>
2023-12-30 11:20:37 +0700

    update diagnosis, training

--- HealthCareApp/HealthCareApp/ViewModels/AddExerciseViewModel.cs
+++ HealthCareApp/HealthCareApp/ViewModels/AddExerciseViewModel.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using static HealthCareApp.Models.ExerciseModel;
 
@@ -17,17 +19,61 @@ namespace HealthCareApp.ViewModels
     internal class AddExerciseViewModel : BaseViewModel
     {
         public ICommand ExitBtnCommand { get; set; }
+        public ICommand SuggestBtnCommand { get; set; }
+        public ICommand MusleSelectedItemChangedCommand { get; set; }
+        public ICommand TypeSelectedItemChangedCommand { get; set; }
+        public ICommand DifficultySelectedItemChangedCommand { get; set; }
+        public string nameMerge;
+        public string NameMerge { get { return nameMerge; } set { nameMerge = value; OnPropertyChanged(nameof(NameMerge)); } }
+        public string typeBox;
+        public string TypeBox { get { return typeBox; } set { typeBox = value; OnPropertyChanged(nameof(TypeBox)); } }
+
+        public string muscleBox;
+        public string MuscleBox { get { return muscleBox; } set { muscleBox = value; OnPropertyChanged(nameof(MuscleBox)); } }
+
+        public string difficultyBox;
+        public string DifficultyBox { get { return difficultyBox; } set { difficultyBox = value; OnPropertyChanged(nameof(DifficultyBox)); } }
+
+        public List<string> tempMuscleList = new List<string>();
+        public List<string> TempMuscleList { get { return tempMuscleList; } set { tempMuscleList = value; OnPropertyChanged(nameof(TempMuscleList)); } }
+        public List<string> tempTypeList = new List<string>();
+        public List<string> TempTypeList { get { return tempTypeList; } set { tempTypeList = value; OnPropertyChanged(nameof(TempTypeList)); } }
+        public List<string> tempDifficultyList = new List<string>();
+        public List<string> TempDifficultyList { get { return tempDifficultyList; } set { tempDifficultyList = value; OnPropertyChanged(nameof(TempDifficultyList)); } }
+
         public AddExerciseViewModel()
         {
-            ExitBtnCommand=new RelayCommand<Window>((p) => { return true; }, (p) => { p.Close(); });
-            getExercise();
+            AddMuscleList();
+            AddTypeList();
+            AddDifficultyList();
+            ExitBtnCommand = new RelayCommand<Window>((p) => { return true; }, (p) => { p.Close(); });
+            SuggestBtnCommand = new RelayCommand<TextBox>((p) =>
+            {
+                if ((MuscleBox == null || MuscleBox == string.Empty) & (TypeBox == null || TypeBox == string.Empty) & (DifficultyBox == null || DifficultyBox == string.Empty))
+                    return false;
+                return true;
+            }, (p) => {
+
+            });
+            MusleSelectedItemChangedCommand = new RelayCommand<ComboBox>((p) => { return (p == null) ? false : true; }, (p) =>
+            {
+                MuscleBox = p.Text;
+            });
+            TypeSelectedItemChangedCommand = new RelayCommand<ComboBox>((p) => { return (p == null) ? false : true; }, (p) =>
+            {
+                TypeBox = p.Text;
+            });
+            DifficultySelectedItemChangedCommand = new RelayCommand<ComboBox>((p) => { return (p == null) ? false : true; }, (p) =>
+            {
+                DifficultyBox = p.Text;
+            });
         }
         string api_key = "2qhorCkNJtS7gPC5veMb0w==vuH8jN3gfZBHCzzh";
         string url = "https://api.api-ninjas.com/v1/exercises?";
         public rootExercises ListExercise;
-        private async void getExercise()
+        private async void getExercise(string name, string type, string muscle, string difficulty)
         {
-            string result = await requestExercise("press", "strength", "biceps", "beginner");
+            string result = await requestExercise(name, type, muscle, difficulty);
         }
         private async Task<string> requestExercise(string name, string type, string muscle, string difficulty)
         {
@@ -63,6 +109,47 @@ namespace HealthCareApp.ViewModels
                     }
             }
             return result;
+        }
+        private void AddMuscleList()
+        {
+            List<string> list = new List<string>();
+            list.Add("abdominals");
+            list.Add("abductors");
+            list.Add("adductors");
+            list.Add("biceps");
+            list.Add("calves");
+            list.Add("chest");
+            list.Add("forearms");
+            list.Add("glutes");
+            list.Add("hamstrings");
+            list.Add("lats");
+            list.Add("lower_back");
+            list.Add("middle_back");
+            list.Add("neck");
+            list.Add("quadriceps");
+            list.Add("traps");
+            list.Add("triceps");
+            TempMuscleList = list;
+        }
+        private void AddTypeList()
+        {
+            List<string> list = new List<string>();
+            list.Add("cardio");
+            list.Add("cardioolympic_weightlifting");
+            list.Add("plyometrics");
+            list.Add("powerlifting");
+            list.Add("strength");
+            list.Add("stretching");
+            list.Add("strongman");
+            TempTypeList = list;
+        }
+        private void AddDifficultyList()
+        {
+            List<string> list = new List<string>();
+            list.Add("beginner");
+            list.Add("intermediate");
+            list.Add("expert");
+            TempDifficultyList = list;
         }
     }
 }
--- HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
+++ HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50070e721a0af81b414ed984b1adddf6a818f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
