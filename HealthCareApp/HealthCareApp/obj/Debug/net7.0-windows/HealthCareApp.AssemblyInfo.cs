9d5863694e5969996a4344d7ef25d203640bb9a8
HGB3009 <22520103@gm.uit.edu.vn>
2023-12-30 12:08:40 +0700

    update addmealview

--- HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
+++ HealthCareApp/HealthCareApp/obj/Debug/net7.0-windows/HealthCareApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cca7780325f4c3d9d9d39c39c0f41114b996914")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCareApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
