985a129a0ad3c762aeaa6a7cac8f9848be59a98d
HGB3009 <22520103@gm.uit.edu.vn>
2024-01-01 02:29:05 +0700

    Update Sleep Screen

--- HealthCareApp/HealthCareApp/ViewModels/SleepViewModel.cs
+++ HealthCareApp/HealthCareApp/ViewModels/SleepViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using HealthCareApp.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HealthCareApp.ViewModels
 {
     public class SleepViewModel : BaseViewModel
     {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+        public ICommand ChartShowCommand { get; set; }
+        public ICommand HistoryCommand { get; set; }
+        public ICommand SleepTrackerCommand { get; set; }
+        public ICommand LoadWindowCommand { get; set; }
+        public SleepViewModel()
+        {
+            ChartShowCommand = new RelayCommand(SleepChart);
+            HistoryCommand = new RelayCommand(SleepHistory);
+            CurrentView = new SleepChartView();
+        }
+        private void SleepChart(object obj) => CurrentView = new SleepChartView();
+        private void SleepHistory(object obj) => CurrentView = new SleepHistoryView();
     }
 }
