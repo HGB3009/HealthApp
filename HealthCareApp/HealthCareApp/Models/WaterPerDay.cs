84f09113cc6330601c4a70d8025b48e020693b18
HGB3009 <22520103@gm.uit.edu.vn>
2024-01-02 05:50:00 +0700

    update water process

---
+++ HealthCareApp/HealthCareApp/Models/WaterPerDay.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareApp.Models
+{
+    public class WaterPerDay
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+        [BsonElement("Day")]
+        public string Day { get; set; }
+        [BsonElement("AmountOfWater")]
+        public double AmountOfWater { get; set; }
+    }
+}
